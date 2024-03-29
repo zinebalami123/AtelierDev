77023300760dd6052884cd4461f085f1d31b3b7b
zineba049@gmail.com <zineba049@gmail.com>
2024-03-29 14:26:53 +0000

    First commit

---
+++ AtelierDevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AtelierDevOps.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
